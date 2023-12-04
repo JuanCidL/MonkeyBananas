74d200644dc599ccd56d77dd043bd7c2ce0b0163
MatyCabreraa <matiasscabreraa@gmail.com>
2023-12-04 11:12:35 -0300

    Nuevo nivel actualizado
    Se cambió el fondo por uno grande, se agregaron unos pinchos más para hacerlo un poco más dificil y se bajaron las alturas de las plataformas

--- scenes/levels/maps/another_level.gd
+++ scenes/levels/maps/another_level.gd
@@ -3,6 +3,6 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Global.set_gun_picked(true) # Replace with function body.
-
+	Global.set_gun_picked(true)
+	Global.set_bullet_counter(-1)
 
